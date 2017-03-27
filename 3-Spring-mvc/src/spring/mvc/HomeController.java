c1a1bfb166cfe14b27bde034358b936827ec8ac9
Surreallistic <iluzion5@o2.pl>
2017-03-27 11:18:24 +0200

    spring mvc - remember about dot at web.xml ...

---
+++ 3-Spring-mvc/src/spring/mvc/HomeController.java
@@ -0,0 +1,17 @@
+package spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Łukasz Sroczyński on 27.03.2017.
+ */
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String HomePage() {
+        return "main-page";
+    }
+}
